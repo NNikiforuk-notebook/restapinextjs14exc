9f00638cc9440a5e606b780b68b57939d7903feb
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-01-25 16:03:33 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const harryPotter = [
+	{
+		id: 1,
+		name: "Harry Potter",
+		age: 16,
+		house: "Gryffindor",
+	},
+	{
+		id: 2,
+		name: "Ron Weasley",
+		age: 16,
+		house: "Gryffindor",
+	},
+	{
+		id: 3,
+		name: "Kreacher",
+		age: 18,
+		house: "Borrow",
+	},
+	{
+		id: 4,
+		name: "Snape",
+		age: 24,
+		house: "Slytherin",
+	},
+];
